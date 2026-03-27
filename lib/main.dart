1fe0726f56437fedb9f04ad9f7840e7003ba2cc7
David <david.marvel.programer.2409@gmail.com>
2026-03-27 12:18:34 -0300

    Comentario

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
 class MyApp extends StatelessWidget {
-const MyApp({Key? key}) : super(key: key);
-@override
-Widget build(BuildContext context) {
-  return MaterialApp(
-    title: 'Buscaminas',
-    theme: ThemeData(primarySwatch: Colors.blue),
-    home: const Text('Hello, World!'), // Apuntamos a nuestra nueva pantalla
-  );
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Buscaminas",
+      theme: ThemeData(
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const Text("HolaMundo"),
+    );
+  }
 }
-}
+
