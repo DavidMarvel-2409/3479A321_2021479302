2070295a92192b3885b2fd0d38aee0aaae543194
DavidMarvel2409 <david.marvel.programer.2409@gmail.com>
2026-05-08 11:51:28 -0400

    agregado modelo CellModel

---
+++ lib/models/cell_model.dart
@@ -0,0 +1,12 @@
+class CellModel {
+  final int index;
+  bool isBomb;
+  bool isRevealed;
+  bool isFlagged;
+  CellModel({
+    required this.index,
+    this.isBomb = false,
+    this.isRevealed = false,
+    this.isFlagged = false,
+  });
+}
