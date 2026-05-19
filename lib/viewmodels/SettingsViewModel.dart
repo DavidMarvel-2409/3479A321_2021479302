cdc4faca7b28d31301e398e644e1466d62ad7b60
DavidMarvel2409 <david.marvel.programer.2409@gmail.com>
2026-05-19 15:43:40 -0400

    Agrega SettingsViewModel

---
+++ lib/viewmodels/SettingsViewModel.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import '../core/services/storage_service.dart';
+
+class SettingsViewModel extends ChangeNotifier {
+  String _username = '';
+  String _difficulty = 'Fácil';
+
+  String get usermane => _username;
+  String get difficulty => _difficulty;
+
+  int get gridSize {
+    if (_difficulty == 'Fácil') return 8;
+    if (_difficulty == 'Medio') return 10;
+    return 12;
+  }
+
+  SettingsViewModel() {
+    _loadSettings();
+  }
+  void _loadSettings() {
+    _username = StorageService.getUsername();
+    _difficulty = StorageService.getDifficulty();
+  }
+
+  void refreshSettings() {
+    _loadSettings();
+    notifyListeners();
+  }
+}
